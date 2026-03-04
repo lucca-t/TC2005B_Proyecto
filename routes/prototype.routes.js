26bb8dd9c81a17d548108774d9c3e5565d1c9594
Joel <joeleoj2006@gmail.com>
2026-03-04 15:33:18 -0600

    Actualizar package.json, fix server

--- routes/prototype.routes.js
+++ routes/prototype.routes.js
@@ -4,3 +4,5 @@ const router = express.Router();
 const prototypeController = require('../controllers/prototype.controller');
 
 router.get('/prototype', prototypeController.get_prototype);
+
+module.exports = router;    
