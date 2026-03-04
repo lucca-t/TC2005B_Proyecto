b014bd0a166590a3a4d3b78db236bd6d93a72f18
JakeLegacyDECLA <a01712823@tec.mx>
2026-03-04 13:34:48 -0600

    Inicio de Navegación Prototipo

---
+++ routes/prototype.routes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
+const prototypeController = require('../controllers/prototype.controller');
+
+router.get('/prototype', prototypeController.get_prototype);
