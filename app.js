7744231b5d6da9b603e1ef3ec4cd2bd5d112106b
Joel <joeleoj2006@gmail.com>
2026-03-04 15:33:18 -0600

    Actualizar package.json, fix server

--- app.js
+++ app.js
@@ -9,11 +9,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Rutes
-
-const ruta_prototype = require('./routes/prototype.routes');
-app.use('/prototype', ruta_prototype);
-
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -21,8 +16,13 @@ const session = require('express-session');
 app.use(session({
     secret: 'mysecretkey',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
 }));
+
+//Rutes
+
+const ruta_prototype = require('./routes/prototype.routes');
+app.use('/prototype', ruta_prototype);
 
 app.use((request, response, next) => {
     response.status(404).send('404 Not Found');
