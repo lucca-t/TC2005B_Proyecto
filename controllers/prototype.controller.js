ebe474c964704e27c226193bcf3984f91d84fdad
Joel <joeleoj2006@gmail.com>
2026-03-05 16:17:25 -0600

    Added routes for 3 types of reports

--- controllers/prototype.controller.js
+++ controllers/prototype.controller.js
@@ -5,3 +5,21 @@ exports.get_prototype = ((request, response, next) => {
         username: request.session.username || '',
     });
 });
+
+exports.get_user_report = ((request, response, next) => {
+    response.render('userReport', {
+        username: request.session.username || '',
+    });
+});
+
+exports.get_team_report = ((request, response, next) => {
+    response.render('teamReport', {
+        username: request.session.username || '',
+    });
+});
+
+exports.get_project_report = ((request, response, next) => {
+    response.render('projectReport', {
+        username: request.session.username || '',
+    });
+});
