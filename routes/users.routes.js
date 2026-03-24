80604ac282aa23ecad381165437c52bf19daefa3
JakeLegacyDECLA <a01712823@tec.mx>
2026-03-24 13:03:27 -0600

    Feat: Developed Homepage

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -5,5 +5,6 @@ const usersController = require('../controllers/users.controller');
 router.get('/login', usersController.get_login);
 router.post('/login', usersController.post_login);
 router.get('/logout', usersController.logout);
+router.get('/home', usersController.get_home);
 
 module.exports = router;
