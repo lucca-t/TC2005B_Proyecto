13d2e99e8eb01dcddd1001baea9b57fe6563313a
JakeLegacyDECLA <a01712823@tec.mx>
2026-03-26 00:12:47 -0600

    Fix: fixed web navegation between options in navbar from homepage

---
+++ routes/users.routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const usersController = require('../controllers/users.controller');
+
+router.get('/list', usersController.get_list); 
+router.get('/edit', usersController.get_edit); //Here we should do a way to edit a specific user
+router.get('/add', usersController.get_add);
+
+module.exports = router;
