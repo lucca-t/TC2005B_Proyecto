e8be3640ba2404e000f5c30499ba39eba7f669ef
JakeLegacyDECLA <a01712823@tec.mx>
2026-03-26 19:11:18 -0600

    Feat: LogIn now functional

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ app.use((req, res, next) => {
 
 const route_homepage = require('./routes/homepage.routes');
 const route_login = require('./routes/login.routes');
+const loginController = require('./controllers/login.controller');
 const route_reports = require('./routes/reports.routes');
 const route_teams = require('./routes/teams.routes');
 const route_users = require('./routes/users.routes');
@@ -48,7 +49,7 @@ const route_standup = require('./routes/daily_standup.routes');
 
 app.use('/homepage', route_homepage);
 app.use('/login', route_login);
-app.use('/logout', route_login);
+app.get('/logout', loginController.get_logout);
 app.use('/reports', route_reports);
 app.use('/teams', route_teams);
 app.use('/users', route_users);
