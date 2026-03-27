abdf00c2f09413944e6b3ec14ec59d87221ceb79
Joel <joeleoj2006@gmail.com>
2026-03-26 19:32:31 -0600

    Feature: Add new team and team list view

--- routes/teams.routes.js
+++ routes/teams.routes.js
@@ -6,5 +6,6 @@ const teamsController = require('../controllers/teams.controller');
 router.get('/list', isAuth, teamsController.get_list);
 router.get('/edit', isAuth, teamsController.get_edit);
 router.get('/add', isAuth, teamsController.get_add);
+router.post('/add',isAuth, teamsController.post_add);
 
 module.exports = router;
