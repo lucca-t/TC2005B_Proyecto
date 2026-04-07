bce8100ebdcdb35a647c53da31ea258897afdbcf
lucca-t <ninja.spinji@gmail.com>
2026-04-07 12:18:25 -0600

    docs(fr01): fixed FR01 sequence diagram APPROVED BY LALO, generate password hash util

---
+++ util/generate-password-hash.js
@@ -0,0 +1,17 @@
+const bcrypt = require('bcrypt');
+
+const plainPassword = process.argv[2];
+
+if (!plainPassword) {
+    console.error('Usage: node util/generate-password-hash.js <plain_password>');
+    process.exit(1);
+}
+
+bcrypt.hash(plainPassword, 12)
+    .then((hash) => {
+        console.log(hash);
+    })
+    .catch((error) => {
+        console.error('Failed to generate hash:', error.message);
+        process.exit(1);
+    });
