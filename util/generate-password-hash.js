012069bd17d04d93019a3bda83fd0d8f07074702
lucca-t <ninja.spinji@gmail.com>
2026-04-10 00:23:24 -0600

    doc (FR03): Created FR 03 for projects list

--- util/generate-password-hash.js
+++ util/generate-password-hash.js
@@ -3,23 +3,23 @@ const bcrypt = require('bcrypt');
 const plainPassword = process.argv[2];
 
 if (!plainPassword) {
-  console.error('Usage: node util/generate-password-hash.js <plain_password>');
-  process.exit(1);
+    console.error('Usage: node util/generate-password-hash.js <plain_password>');
+    process.exit(1);
 }
 
 bcrypt.hash(plainPassword, 12)
     .then((hash) => {
-      console.log(hash);
+        console.log(hash);
     })
     .catch((error) => {
-      console.error('Failed to generate hash:', error.message);
-      process.exit(1);
+        console.error('Failed to generate hash:', error.message);
+        process.exit(1);
     });
 
 /**
  * npm run hash:password -- 'password'
- *
+ * 
  * INSERT INTO User (email, password, full_name, slack_handle, slack_id)
 VALUES ('admin@mail.com', 'HASH', 'admin', '@admin', '1234');
- *
+ * 
  */
