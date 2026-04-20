36759c4e3b8a9e0a6a027753fce76dd444c20cbe
JakeLegacyDECLA <a01712823@tec.mx>
2026-04-20 10:56:52 -0600

    Feat: RBAC implemented, now all permissions are correct

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -5,27 +5,28 @@ const {authorize, ROLES} = require('../util/rbac');
 const usersController = require('../controllers/users.controller');
 
 const adminOnly = authorize(ROLES.ADMIN);
+const adminAndLead = authorize(ROLES.ADMIN, ROLES.LEAD);
 const allRoles = authorize(ROLES.ADMIN, ROLES.LEAD, ROLES.MEMBER);
 
-// FR-07: Consultar Usuario - Admin, Lead, Member
+// FR-07: Read User - Admin, Lead
 router.get('/list', isAuth, allRoles, usersController.get_list);
 
-// FR-05: Registrar Usuario - Admin
+// FR-05: Register User - Admin
 router.get('/add', isAuth, adminOnly, usersController.get_add);
 router.post('/add', isAuth, adminOnly, usersController.post_add);
 
-// FR-08: Editar Usuario - Admin
+// FR-08: Edit User - Admin
 router.get('/edit/:email', isAuth, adminOnly, usersController.get_edit);
 router.post('/edit/:email', isAuth, adminOnly, usersController.post_edit);
 
-// FR-17: Vincular rol a usuario - Admin
+// FR-17: Link role to user - Admin
 router.get('/role/:userId', isAuth, adminOnly, usersController.get_role);
 router.post('/role/:userId', isAuth, adminOnly, usersController.post_role);
 
-// FR-06: Eliminar Usuario - Admin
+// FR-06: Delete User - Admin
 router.post('/delete/:userId', isAuth, adminOnly, usersController.post_delete);
 
-// FR-18: Registrar reporte de miembro - Admin, Lead, Member
+// FR-18: Register member report - Admin, Lead, Member
 router.get('/report/:userId', isAuth, allRoles, usersController.get_report);
 router.post('/report/:userId', isAuth, allRoles, usersController.post_report);
 
